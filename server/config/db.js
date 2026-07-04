7a9818c46f9fa204b3290fb61aae8e8806870a31
Matu gutierrez <matugutierrez444@gmail.com>
2026-07-04 05:52:51 -0300

    fix

--- server/config/db.js
+++ server/config/db.js
@@ -4,10 +4,7 @@ const { MONGO_URI } = require('./env');
 let gridfsBucket = null;
 
 async function connectDB() {
-  const conn = await mongoose.connect(MONGO_URI, {
-    tls: true,
-    tlsInsecure: true,
-  });
+  const conn = await mongoose.connect(MONGO_URI);
   console.log('MongoDB connected:', conn.connection.host);
 
   gridfsBucket = new mongoose.mongo.GridFSBucket(conn.connection.db, {
